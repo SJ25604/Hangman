2ede81f89b6498452f4530942165fdc43f54c84e
Mateusz <malekmateusz00128@wp.pl>
2018-05-13 21:55:56 +0200

    +-zmiana dwuwymiarowego wektora wektorow charow na wektor struktur +-wieksza liczba mozliwych pomylek
    Slowa w koncu wyswietlaja sie z polskimi znakami, ale wciaz nie mozna polskich znakow wprowadzic z klawiatury

--- main.cpp
+++ main.cpp
@@ -6,6 +6,45 @@
 #include <fstream>
 #include <random>
 #include <chrono>
+#include <locale>
+
+//Structura
+
+struct ZnakZeStanem{
+private:
+    char znak;
+//    true - odgadniety
+    bool stan;
+//    symbol zasloniecia
+    char symbolZasloniecia = '_';
+
+public:
+
+    ZnakZeStanem(char znak) : znak(znak){
+        stan = false;
+    }
+
+    char ustalZnakZeStanem(){
+        if(stan){
+            return znak;
+        }
+        else{
+            return symbolZasloniecia;
+        }
+    }
+
+    int aktualizujStan(char litera){
+        if(!stan && znak == litera){
+                stan = true;
+                return 1;
+        }
+        return 0;
+    }
+
+    char zwrocStan(){
+        return stan;
+    }
+};
 
 std::vector<std::string> zaladujSlowaZBazy(){
         using namespace std;
@@ -15,7 +54,7 @@ std::vector<std::string> zaladujSlowaZBazy(){
         string wiersz;
 
 //        obsluga pliku i zapisanie w bazie
-        plik.open("../slowa/slowa.txt");
+        plik.open("../slowa/slowaMSDOS.txt");
         while (getline(plik, wiersz )){
             slowa.push_back(wiersz);
         }
@@ -24,23 +63,26 @@ std::vector<std::string> zaladujSlowaZBazy(){
         return slowa;
 }
 
+//  funkcja losujaca slowo z wczytanej bazy
     std::string wylosujSlowo(/*std::vector<std::string> slowa*/){
-        //funkcja losujaca slowo z wczytanej bazy
+
         std::vector<std::string> slowa = zaladujSlowaZBazy();
 
 //        rand
 /*        srand(time(nullptr));
         int losowa = rand() % slowa.size();*/
 
-//        nowszt i podobno lepszy system generowania liczb pseudolosowych dla c++11
-        long long int seed = std::chrono::system_clock::now().time_since_epoch().count();
+/*        nowszy i podobno lepszy system generowania liczb pseudolosowych
+        dla c++11*/
+        long long int seed =
+                std::chrono::system_clock::now().time_since_epoch().count();
         std::default_random_engine generator (seed);
         std::uniform_int_distribution<int> distribution(0, slowa.size());
         int losowa = distribution(generator);
 
         std::cout<<losowa<<std::endl<<slowa.size()<<std::endl;
         std::string wylosowane = slowa.at(losowa);
-        return wylosowane;   //randint jest wspierany od c++14 :(, więc jednak standardowe rozwiązanie
+        return wylosowane;
 }
 
     void graj() {
@@ -48,19 +90,27 @@ std::vector<std::string> zaladujSlowaZBazy(){
     do{
 //    deklaracja zmiennych
         bool odgadniete = false;
-        int liczba_dozwolonych_pomylek = 5;
-        char znakZakrycia = '_';
+        int liczba_dozwolonych_pomylek = 10;
         std::vector<char> literySprawdzone;
 
-//    losowanie slowa i zamaiana na vector vectorow charow z informacja o stanie w drugim wymiarze
+/*        losowanie slowa i zamaiana na vector vectorow charow z informacja
+        o stanie w drugim wymiarze*/
         std::string slowo = wylosujSlowo();
-//        std::cout << slowo << std::endl;       //tylko na chwile, aby sprawdzic poprawnosc funkcji losujacej
-        std::vector<char> slowoZnaki(slowo.begin(), slowo.end());
+
+        std::cout << slowo << std::endl;
+// tylko na chwile, aby sprawdzic poprawnosc funkcji losujacej
+
+        std::vector<ZnakZeStanem> slowoZnaki;
+        for(auto znak : slowo){
+//            ZnakZeStanem doDodania = ZnakZeStanem(znak);
+            slowoZnaki.emplace_back(znak);
+        }
+/*        std::vector<char> slowoZnaki(slowo.begin(), slowo.end());
         std::vector<std::vector<char>> slowoZnakiOdgadniete;
         for (auto litera : slowoZnaki) {
             std::vector<char> doWstawienia = {litera, '0'};
             slowoZnakiOdgadniete.push_back(doWstawienia);
-        }
+        }*/
 
 //
         //Jakas petla zaporowa do obslugi gry
@@ -68,15 +118,15 @@ std::vector<std::string> zaladujSlowaZBazy(){
 
 //           deklaracja zmiennych do sprawdzania wystepowania litery w slowie
             char litera;
-            bool trafiona = false;
 
 //            wypisanie slowa
-            for (auto literaSlowa : slowoZnakiOdgadniete) {
-                if (literaSlowa[1] == '1') {
+            for (auto literaSlowa : slowoZnaki) {
+/*                if (literaSlowa[1] == '1') {
                     std::cout << literaSlowa[0] << ' ';
                 } else {
                     std::cout << znakZakrycia << ' ';
-                }
+                }*/
+                std::cout<<literaSlowa.ustalZnakZeStanem()<<' ';
             }
 
 //          prosba o podanie litery i informacja o juz sprawdzonych literach
@@ -84,16 +134,19 @@ std::vector<std::string> zaladujSlowaZBazy(){
             for (auto literaSprawdzona : literySprawdzone) {
                 std::cout << literaSprawdzona << " ";
             }
-            std::cout << "\nMozliwych pomylek: " << liczba_dozwolonych_pomylek << "\nWprowadz litere: " << std::endl;
+            std::cout << "\nMozliwych pomylek: " << liczba_dozwolonych_pomylek
+                      << "\nWprowadz litere: " << std::endl;
 
-//            przyjecie litery i sprawdzenie, czy nie wprowadzono pustego znaku lub znaku spoza zakresu liter
+/*          przyjecie litery i sprawdzenie, czy nie wprowadzono pustego znaku
+            lub znaku spoza zakresu liter*/
             do {
                 std::cin >> litera;
-            } while (!((litera >= 'a' && litera <= 'z') || (litera >= 'A' && litera <= 'Z')));
+                _tolower(litera);
+            } while (!(litera >= 'a' && litera <= 'z'));
 
 
-//            konwersja wprowadzonej litery na mala, poniewaz w bazie slowa skladaja sie z malych liter
-            _tolower(litera);
+/*            konwersja wprowadzonej litery na mala,
+            poniewaz w bazie slowa skladaja sie z malych liter*/
 
 //            sprawdzenie
 /*            for(int i = 0; i < slowoZnaki.size(); i++){
@@ -107,20 +160,24 @@ std::vector<std::string> zaladujSlowaZBazy(){
                     trafiona = true;
                 }
             }*/
-            for (int i = 0; i < slowoZnakiOdgadniete.size(); i++) {
+/*            for (int i = 0; i < slowoZnakiOdgadniete.size(); i++) {
                 if (slowoZnakiOdgadniete[i][0] == litera) {
                     slowoZnakiOdgadniete[i][1] = '1';
                     trafiona = true;
                 }
+            }*/
+            int liczbaTrafien = 0;
+            for (auto & znak : slowoZnaki){
+                liczbaTrafien += znak.aktualizujStan(litera);
             }
 
-//            aktualizacja warunkow petli zaporowej
-//            w przypadku trafienia literki gra nie zmniejsza dostepnej liczby pomylek,
-//            ale sprawdza, czy jest jeszcze cos do zgadywania
-            if (trafiona) {
+/*            aktualizacja warunkow petli zaporowej
+            w przypadku trafienia literki gra nie zmniejsza dostepnej
+            liczby pomylek, ale sprawdza, czy jest jeszcze cos do zgadywania*/
+            if (liczbaTrafien) {
                 odgadniete = true;
-                for (auto literaSlowa : slowoZnakiOdgadniete) {
-                    if (literaSlowa[1] != '1') {
+                for (auto literaSlowa : slowoZnaki) {
+                    if (!literaSlowa.zwrocStan()) {
                         odgadniete = false;
                     }
                 }
@@ -134,29 +191,35 @@ std::vector<std::string> zaladujSlowaZBazy(){
 
 
         }
-        //w petli powinno byc cos, co bedzie kontrolowalo stopien odgadniecia slowa
-        //(
-        //tu potrzeba kontroli wystepowania danej litery
-        //[petla po tablicy, z porownaniem kazdej litery do podanej przez gracza]
-        //obsluga podanych juz liter za pomoca vectora charow,
-        //podstawianie "_" pod nieodgadniete litery,
-        //byc moze przydatna bedzie tu petla dwuwymiarowa
-        //w pierwszym wymiarze litery, w drugim informacja o zakryciu/odkryciu
-        // ),
-        //pozostala liczbe mozliwych pomylek,
-        //
-        //po petli analiza wyniku -> wygrana/przegrana, odpowiedni komunikat, możliwosc ponownej gry
+/*        w petli powinno byc cos, co bedzie kontrolowalo stopien
+ *        odgadniecia slowa
+        (
+        tu potrzeba kontroli wystepowania danej litery
+        [petla po tablicy, z porownaniem kazdej litery do podanej przez gracza]
+        obsluga podanych juz liter za pomoca vectora charow,
+        podstawianie "_" pod nieodgadniete litery,
+        byc moze przydatna bedzie tu petla dwuwymiarowa
+        w pierwszym wymiarze litery, w drugim informacja o zakryciu/odkryciu
+         ),
+        pozostala liczbe mozliwych pomylek,
+
+        po petli analiza wyniku -> wygrana/przegrana, odpowiedni komunikat,
+        możliwosc ponownej gry*/
 
         if (odgadniete) {
-            std::cout << "Brawo! odgadniete slowo: " << slowo << "." << std::endl;
+            std::cout << "Brawo! odgadniete slowo: "
+                         << slowo << "." << std::endl;
         } else {
-            std::cout << "Niestety, nie udalo Ci sie. Slowo, ktore probowales odgadnac to: " << slowo << '.'
-                      << std::endl;
+            std::cout << "Niestety, nie udalo Ci sie. "
+                         "Slowo, ktore probowales odgadnac to: "
+                         << slowo << '.' << std::endl;
         }
-        std::cout << "Czy chcesz zagrac jeszcze raz?\nt -> tak\nn -> nie" << std::endl;
+        std::cout << "Czy chcesz zagrac jeszcze raz?\nt -> tak\nn -> nie"
+                     << std::endl;
         do {
             std::cin >> jeszczeRaz;
-        } while (!(jeszczeRaz == 't' || jeszczeRaz == 'T' || jeszczeRaz == 'n' || jeszczeRaz == 'N'));
+            _tolower(jeszczeRaz);
+        } while (!(jeszczeRaz == 't' || jeszczeRaz == 'n'));
     }while(jeszczeRaz == 't');
 }
 
